0f4d6b42e3e467637a49f6554925c8270c1ec7d2
NzNomaxe <Alkatrac5392@gmail.com>
2020-11-30 14:12:35 +1000

    Переключение между этажами

---
+++ Assets/Scripts/Floors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Floors : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> floors = new List<GameObject>();
+
+    private int currentFloorIndex = 0;
+
+    public void SetActiveNextFloor(bool up)
+    {
+        if (up)
+        {
+            if (currentFloorIndex - 1 >= 0)
+            {
+                floors[currentFloorIndex].SetActive(false);
+                floors[--currentFloorIndex].SetActive(true);
+            }
+        }
+        else
+        {
+            if (currentFloorIndex + 1 < floors.Count)
+            {
+                floors[currentFloorIndex].SetActive(false);
+                floors[++currentFloorIndex].SetActive(true);
+            }
+        }
+    }
+}
