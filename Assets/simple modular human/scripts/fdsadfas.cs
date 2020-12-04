995dd1ccfef7795698342183fb811bdacb947c77
NzNomaxe <Alkatrac5392@gmail.com>
2020-12-04 22:23:09 +1000

    Добавлена надпись ВГУЭС

---
+++ Assets/simple modular human/scripts/fdsadfas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveNPC : MonoBehaviour
+{
+    public float dist;
+
+    public float speed;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed);
+        Raycast();
+
+
+    }
+
+    private void Raycast()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, dist))
+        {
+            transform.Rotate(180, 0, 0);
+        }
+    }
+}
