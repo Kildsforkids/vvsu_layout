348969e20f4fcd9dee0760c8747e38f226e06008
glebsmart <glebka.martynov.2001@mail.ru>
2020-12-05 22:44:41 +1000

    Скрипт для переключения этажей обновлён

--- Assets/Scripts/Floors.cs
+++ Assets/Scripts/Floors.cs
@@ -3,27 +3,49 @@ using UnityEngine;
 
 public class Floors : MonoBehaviour
 {
-    [SerializeField]
-    private List<GameObject> floors = new List<GameObject>();
+    private List<GameObject[]> floors = new List<GameObject[]>();
 
     private int currentFloorIndex = 0;
 
+    private void Start()
+    {
+        floors.Add(GameObject.FindGameObjectsWithTag("floor7"));
+        floors.Add(GameObject.FindGameObjectsWithTag("floor6"));
+        floors.Add(GameObject.FindGameObjectsWithTag("floor5"));
+        floors.Add(GameObject.FindGameObjectsWithTag("floor4"));
+    }
     public void SetActiveNextFloor(bool up)
     {
         if (up)
         {
             if (currentFloorIndex - 1 >= 0)
             {
-                floors[currentFloorIndex].SetActive(false);
-                floors[--currentFloorIndex].SetActive(true);
+                foreach(var element in floors[currentFloorIndex])
+                {
+                    element.SetActive(false);
+                }
+                foreach (var element in floors[--currentFloorIndex])
+                {
+                    element.SetActive(true);
+                }
+                //floors[currentFloorIndex].SetActive(false);
+                //floors[--currentFloorIndex].SetActive(true);
             }
         }
         else
         {
             if (currentFloorIndex + 1 < floors.Count)
             {
-                floors[currentFloorIndex].SetActive(false);
-                floors[++currentFloorIndex].SetActive(true);
+                foreach (var element in floors[currentFloorIndex])
+                {
+                    element.SetActive(false);
+                }
+                foreach (var element in floors[++currentFloorIndex])
+                {
+                    element.SetActive(false);
+                }
+                //floors[currentFloorIndex].SetActive(false);
+                // floors[++currentFloorIndex].SetActive(true);
             }
         }
     }
