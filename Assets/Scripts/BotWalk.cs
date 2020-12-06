ec4d6e1920bed844e5e2a6eb469d7e7088ac9d46
NzNomaxe <Alkatrac5392@gmail.com>
2020-12-06 23:42:14 +1000

    Финальный вид на 07.12.2020

---
+++ Assets/Scripts/BotWalk.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class BotWalk : MonoBehaviour
+{
+    
+    [SerializeField]
+    private List<GameObject> floor = new List<GameObject>();
+    Vector3 _targetPoint;
+    private float _speed = 1.0f;
+    private int i = 0;
+    const double PI = 3.14159265;
+
+
+    void Start()
+    {
+        _targetPoint = floor[i].transform.position; // Указываешь нужные координаты
+        double tg = Mathf.Atan((floor[i].transform.position.x - transform.position.x) / (floor[i].transform.position.z - transform.position.z)) * 180.0 / PI;
+        if (floor[i].transform.position.z < transform.position.z)
+            tg += 180;
+        transform.rotation = Quaternion.Euler(0, (float)tg, 0);
+
+    }
+
+    void FixedUpdate()
+    {
+        
+
+        MoveObj(); // Вызываем метод для движения, вызов происходит каждый фрейм или что то вроде того
+        if (transform.position == _targetPoint)
+        {
+            i++;
+            _targetPoint = floor[i].transform.position;
+            double tg = Mathf.Atan((floor[i].transform.position.x - transform.position.x) / (floor[i].transform.position.z - transform.position.z))*180.0/PI;
+            if (floor[i].transform.position.z < transform.position.z)
+                tg += 180;
+            transform.rotation= Quaternion.Euler(0,(float)tg,0);
+        }
+        if (i+1 == floor.Count)
+            i = 0;
+    }
+
+    void MoveObj()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPoint, _speed);
+              //Двигаем объект с помощью метода MoveTowards, в скобках слева на право 1. Текущее положение, 2. Точка назначения, 3. скорость
+
+    }
+}
---
+++ Assets/Scripts/action_UI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class action_UI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gameObject_2;
+
+    public void Click()
+    {
+        if (gameObject.activeSelf == true)
+        {
+
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+}
